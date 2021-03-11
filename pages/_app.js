446cfa723e2463059f389712d502c03cb14c61db
phyyou <gydudwls@gmail.com>
2021-03-11 05:11:08 +0000

    add SEO

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,22 @@ import Head from "next/head";
 import Router from "next/router";
 import theme from "../src/components/theme";
 import NProgress from "nprogress";
+import { SEO } from "../src/components/SEO";
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <SEO />
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const PRODUCTION_URL = 'https://nasomedia.com';
+
+/**
+ * SEO constants
+ */
+ export const SEO_TITLE = 'NASOMEDIA';
+ export const SEO_DESCRIPTION =
+   '';
+ export const SEO_KEYWORDS =
+   '나소미디어';
+ export const SEO_OG_IMAGE = '/icons/nasoicon.png';
+ export const SEO_TWITTER_IMAGE = '/icons/nasoicon.png';
