e0c65a72829962fefc39310cfb836f268aa95aa9
phyyou <gydudwls@gmail.com>
2021-03-27 18:08:42 +0000

    kjj

--- src/components/ViewerMain/index.js
+++ src/components/ViewerMain/index.js
@@ -1,20 +1,16 @@
 const example_data =
   "https://imges-ext-1.discordapp.net/external/lN6Rx6VmOoo2cTlW3r2UiqHjghKf7uRGH15LucjfrDA/https/blog-imgs-135.fc2.com/n/a/s/nasonasofc123456/1_20210128010214540.jpg?width=493&height=701";
 
-import { Container, Flex, Img } from "@chakra-ui/react";
-import NextImage from "next/image";
+import { Container, Flex, Image } from "@chakra-ui/react";
 import React, { useCallback } from "react";
 
 const ViewerMain = () => {
-  const handleImgError = useCallback((e) => {
-    e.target.src = "/noimage.png";
-  }, []);
   return (
     <Container maxW={"2xl"} padding={0}>
       <Flex flexDirection={"column"}>
-        <Img src={example_data} onError={handleImgError}></Img>
-        <Img src={example_data}></Img>
-        <Img src={example_data}></Img>
+        <Image src={example_data} fallbackSrc="/noimage.png"></Image>
+        <Image src={example_data} fallbackSrc="/noimage.png"></Image>
+        <Image src={example_data} fallbackSrc="/noimage.png"></Image>
       </Flex>
     </Container>
   );
