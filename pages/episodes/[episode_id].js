f17c77987f87eed220b838824ae44cfa968751a7
phyyou <gydudwls@gmail.com>
2021-03-27 18:19:21 +0000

    import const

--- pages/episodes/[episode_id].js
+++ pages/episodes/[episode_id].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
-import ViewerLayout from "../../src/layout/ViewerLayout";
+import { ViewerLayout } from "../../src/layout/ViewerLayout";
 
 const EpisodeView = () => {
   const router = useRouter();
