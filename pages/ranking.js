344a624724a29372a7cfa361f4a8f7d3f1a4a812
phyyou <gydudwls@gmail.com>
2021-04-01 10:51:48 +0000

    useless text

--- pages/ranking.js
+++ pages/ranking.js
@@ -10,26 +10,6 @@ export default function Ranking() {
       </Head>
       <Stack>
         <Heading>랭킹</Heading>
-        <Text>
-          consectetur adipiscing elit. Vivamus non dictum nisl. Quisque et
-          venenatis nunc, ac tincidunt nibh. Vestibulum commodo posuere
-          interdum. Praesent eget semper velit. In id sollicitudin odio. Sed
-          cursus auctor leo non fermentum. Vivamus lacinia congue elit, at
-          pharetra purus elementum eu. Nunc vel justo sit amet ex aliquam
-          blandit ornare ut felis. Maecenas auctor, lacus ac volutpat malesuada,
-          nibh ante hendrerit diam, vel sodales nunc turpis in nunc. Vivamus
-          dolor sapien, convallis vitae ultricies quis, volutpat ut lacus. Morbi
-          suscipit erat ut dui facilisis, vel pretium ante aliquam. Praesent sed
-          nunc eu odio posuere condimentum vel sit amet dolor. Nulla ac metus
-          nec nibh pulvinar aliquet vitae eu ante. Vivamus pharetra magna quam,
-          sit amet tempor massa maximus vitae. Phasellus laoreet erat non
-          finibus hendrerit. Integer ligula tellus, sodales eget suscipit
-          sagittis, congue eget tortor. In blandit nisi nec sapien eleifend, eu
-          pretium nibh efficitur. Proin in ipsum sed risus tristique mattis vel
-          et magna. Vestibulum eu augue est. Fusce sem nisi, viverra at mauris
-          sit amet, fermentum vehicula leo. Integer accumsan vehicula vehicula.
-          In luctus ullamcorper placerat. In hac habitasse platea dictumst.
-        </Text>
       </Stack>
     </MainLayout>
   );
---
+++ pages/search.js
@@ -0,0 +1,90 @@
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    VStack,
+    Heading,
+    useColorModeValue,
+  } from "@chakra-ui/react";
+  import { useRouter } from "next/router";
+  import Head from "next/head"
+  
+  export default function Login() {
+    const router = useRouter();
+    return (
+      <div>
+        <Head>
+          <title>검색</title>
+        </Head>
+        <Box
+          width={"full"}
+          height={"100vh"}
+          bgGradient={useColorModeValue(
+            "rgba(240, 240, 240, 0.8)",
+            "rgba(26, 32, 44, 0.8)"
+          )}
+          d="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Box
+            boxShadow={"base"}
+            maxW={"2xl"}
+            borderWidth="1px"
+            mx={2}
+            minW={"60px"}
+            w={"sm"}
+            borderRadius="lg"
+          >
+            <VStack
+              zIndex="999"
+              padding={4}
+              direction={"column"}
+              h={"100%"}
+              minH={"sm"}
+              justify={"center"}
+              alignItems={"center"}
+              spacing={5}
+              css={{
+                backgroundColor: useColorModeValue(
+                  "rgba(255, 255, 255, 0.8)",
+                  "rgba(36, 42, 54, 0.8)"
+                ),
+              }}
+            >
+              <Heading>나소미디어</Heading>
+              <Heading
+                css={{
+                  opacity: "100%",
+                }}
+                fontSize="md"
+              >
+                개발중인 기능입니다.
+              </Heading>
+              <ButtonGroup>
+                <Button
+                  onClick={() => {
+                    router.push("/");
+                  }}
+                  colorScheme="purple"
+                  fontWeight={"medium"}
+                >
+                  홈
+                </Button>
+                <Button
+                  onClick={() => {
+                    router.back();
+                  }}
+                  colorScheme="purple"
+                  fontWeight={"medium"}
+                >
+                  뒤로가기
+                </Button>
+              </ButtonGroup>
+            </VStack>
+          </Box>
+        </Box>
+      </div>
+    );
+  }
+  
