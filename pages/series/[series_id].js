505cbf1627dfd899718832e100d49b7d022f06e9
phyyou <gydudwls@gmail.com>
2021-04-02 10:18:06 +0000

    Series Detail

---
+++ pages/series/[series_id].js
@@ -0,0 +1,31 @@
+import { Stack, Heading, Box } from "@chakra-ui/layout";
+import Head from "next/head";
+import SeriesDetailEpisodeList from "../../src/components/Series/SeriesDetailEpisodeList";
+import SeriesDetailHeader from "../../src/components/Series/SeriesDetailHeader";
+import { MainLayout } from "../../src/layout/MainLayout";
+import { readEpisodesBySeries, readSeries } from "../../src/utils/api";
+
+const SeriesDetail = ({ series, episodes }) => {
+  return (
+    <MainLayout>
+      <Head>
+        <title>{series.title}</title>
+      </Head>
+      <Stack direction={{ base: "column", md: "row" }}>
+        <SeriesDetailHeader series={series} />
+        <SeriesDetailEpisodeList episodes={episodes} />
+      </Stack>
+    </MainLayout>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const { series_id } = context.query;
+  const series = await readSeries(series_id);
+  const episodes = await readEpisodesBySeries(series_id);
+  return {
+    props: { series: series, episodes: episodes }, // will be passed to the page component as props
+  };
+}
+
+export default SeriesDetail;
