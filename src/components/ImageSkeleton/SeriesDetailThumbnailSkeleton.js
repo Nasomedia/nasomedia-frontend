c0da1a2cf3ea703d401e7e8faa3775e8b98647f7
phyyou <gydudwls@gmail.com>
2021-04-03 00:50:49 +0000

    skeleton height

--- src/components/ImageSkeleton/SeriesDetailThumbnailSkeleton.js
+++ src/components/ImageSkeleton/SeriesDetailThumbnailSkeleton.js
@@ -2,5 +2,5 @@ import React from "react";
 import { Skeleton } from "@chakra-ui/react";
 
 export const SeriesDetailThumbnailSkeleton = () => {
-  return <Skeleton width={"full"} height={"auto"} />;
+  return <Skeleton width={"full"} height={"xs"} />;
 };
