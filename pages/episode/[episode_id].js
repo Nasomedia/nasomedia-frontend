e0dfd158cd20ba9498fd842c13593f296b95e073
phyyou <gydudwls@gmail.com>
2021-04-13 14:16:49 +0000

    sSEO

--- pages/episode/[episode_id].js
+++ pages/episode/[episode_id].js
@@ -46,32 +46,54 @@ export async function getServerSideProps(context) {
   const images = await readEpisodeImages(episode_id);
   const nextEpisode = await readNextEpisode(episode_id);
   const prevEpisode = await readPrevEpisode(episode_id);
-  const ldJSON = {
-    "@context": "https://schema.org",
-    "@type": "Article",
-    mainEntityOfPage: {
-      "@type": "WebPage",
-      "@id": `https://nasomedia.com/episode/${episode.id}`,
-    },
-    headline: `${series.title} ${episode.title}`,
-    image: [`${episode.thumbnail}`],
-    dateCreated: `${episode.create_at}`,
-    datePublished: `${episode.create_at}`,
-    dateModified: `${episode.update_at}`,
-    author: {
-      "@type": "Person",
-      name: "나소미디어",
-    },
-    publisher: {
-      "@type": "Organization",
-      name: "나소미디어",
-      logo: {
-        "@type": "ImageObject",
-        url: "https://nasomedia.com/icons/nasoicon.png",
+  const ldJSON = [
+    {
+      "@context": "https://schema.org",
+      "@type": "Article",
+      mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": `https://dev.nasomedia.com/episode/${episode.id}`,
+        // "@id": `${BASE_URL}/episode/${episode.id}`,
       },
+      headline: `${series.title} ${episode.title}`,
+      image: [`${episode.thumbnail}`],
+      dateCreated: `${episode.create_at}`,
+      datePublished: `${episode.create_at}`,
+      dateModified: `${episode.update_at}`,
+      author: {
+        "@type": "Person",
+        name: "나소미디어",
+      },
+      publisher: {
+        "@type": "Organization",
+        name: "나소미디어",
+        logo: {
+          "@type": "ImageObject",
+          url: "https://dev.nasomedia.com/icons/nasoicon.png",
+          // "@id": `${BASE_URL}/${SEO_OG_IMAGE}`,
+        },
+      },
+      description: `${series.description}`,
     },
-    description: `${series.description}`,
-  };
+    {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: series.title,
+          item: `https://dev.nasomedia.com/series/${series.id}`,
+          // "@id": `${BASE_URL}/series/${series.id}`,
+        },
+        {
+          "@type": "ListItem",
+          position: 2,
+          name: episode.title,
+        },
+      ],
+    },
+  ];
   return {
     props: {
       episode: episode,
