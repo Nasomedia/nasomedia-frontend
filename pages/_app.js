693fa370649c61babd5b2171b05c4938b80047e8
phyyou <gydudwls@gmail.com>
2021-04-18 11:47:54 +0000

    preconnect font googleapis

--- pages/_app.js
+++ pages/_app.js
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps }) {
       <PWA />
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
