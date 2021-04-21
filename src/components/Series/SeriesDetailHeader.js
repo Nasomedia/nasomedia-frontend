8bc77d7a45fe13ad7bcb07922ecd232b3f44afc2
phyyou <gydudwls@gmail.com>
2021-04-21 11:37:02 +0000

    add bg & blur filter

--- src/components/Series/SeriesDetailHeader.js
+++ src/components/Series/SeriesDetailHeader.js
@@ -6,6 +6,7 @@ import {
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import React from "react";
 import { SeriesDetailThumbnailSkeleton } from "../ImageSkeleton";
@@ -17,15 +18,49 @@ const SeriesDetailHeader = ({ series }) => {
       minW={{ base: "full", sm: "xs" }}
       p={2}
     >
-      <Image
-        src={series.thumbnail}
-        fallback={<SeriesDetailThumbnailSkeleton />}
-        width={"full"}
-        height={"auto"}
-        objectFit="cover"
-        alt={`thumbnail_image-${series.title}`}
-      />
-      <Heading mt={4}>{series.title}</Heading>
+      <Box
+        role={"group"}
+        mb={"4"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        pos={"relative"}
+        zIndex={1}
+      >
+        <Box
+          rounded={"lg"}
+          pos={"relative"}
+          width={"full"}
+          height={"auto"}
+          _after={{
+            transition: "all .3s ease",
+            content: '""',
+            w: "full",
+            h: "full",
+            pos: "absolute",
+            top: 5,
+            left: 0,
+            backgroundImage: `url(${series.thumbnail})`,
+            filter: "blur(15px)",
+            zIndex: -1,
+          }}
+          _hover={{
+            _after: {
+              filter: "blur(20px)",
+            },
+          }}
+        >
+          <Image
+            width={"full"}
+            height={"auto"}
+            objectFit={"cover"}
+            fallback={<SeriesDetailThumbnailSkeleton />}
+            alt={`thumbnail_image-${series.title}`}
+            src={series.thumbnail}
+          />
+        </Box>
+      </Box>
+      <Heading zIndex={10} mt={6}>{series.title}</Heading>
       <Box as="span" color="gray.400" fontSize="sm">
         장르: - | -세 이용가 | 작가: -
       </Box>
