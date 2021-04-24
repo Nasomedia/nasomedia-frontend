5faf1409cccfdbfa1c3a90544a7d886b29a8f7aa
phyyou <gydudwls@gmail.com>
2021-04-24 09:47:08 +0000

    Create: Adsense code

---
+++ src/components/AdSense.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export const AdSense = () => {
+  return (
+    <Head>
+      <script
+        data-ad-client="ca-pub-1638178136808231"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      ></script>
+    </Head>
+  );
+};
