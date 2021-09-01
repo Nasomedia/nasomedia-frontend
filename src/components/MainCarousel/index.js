83c4bd0c60809f805b783edbd1b4a77373858995
Hyoyoung Park <gydudwls@gmail.com>
2021-09-01 17:57:18 +0900

    create: Main Carousel

--- src/components/MainCarousel/index.js
+++ src/components/MainCarousel/index.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import { Container, LinkBox, LinkOverlay, Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import NextLink from "next/link";
+
+const CarouselBox = ({ href, image }) => {
+  return (
+    <LinkBox width={"7xl"} flexShrink={"0"}>
+      <NextLink href={href} passHref>
+        <LinkOverlay>
+          <Image src={image}></Image>
+        </LinkOverlay>
+      </NextLink>
+    </LinkBox>
+  );
+};
 
 export const MainCarousel = () => {
+  const example_list = [
+    { href: "/series/3", image: "/noimage.png" },
+    { href: "/series/3", image: "/noimage.png" },
+    { href: "/series/3", image: "/noimage.png" },
+  ];
   return (
-    <div>
-      
-    </div>
+    <Container
+      maxW={"7xl"}
+      sx={{
+        margin: "0 auto",
+        position: "relative",
+        overflow: "hidden",
+        padding: "0",
+      }}
+    >
+      <Flex overflow={"hidden"} position={"relative"}>
+        {example_list.map((example) => (
+          <CarouselBox href={example.href} image={example.image} />
+        ))}
+      </Flex>
+    </Container>
   );
 };
